6ec2ee21317e0c6f24c34fb9c73b66bc6e45a7e2
Philip Willms <philip@narando.de>
2020-09-19 13:43:18 +0200

    [main] remove top image

--- src/App.js
+++ src/App.js
@@ -10,13 +10,6 @@ import styled from "styled-components";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sickest Data on planet earth, collected and displayed by Adrian and
-          Philip. Stay tuned!!
-        </p>
-      </header>
       <Wrapper>
         <Header header="Temperature" />
         <TempChart />
