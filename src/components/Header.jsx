359a6dfa2f86b99a77d0089fa3515cd864a3ff7a
Philip Willms <philip@narando.de>
2021-02-11 21:24:35 +0100

    feat: add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Header = (props) => <Head>{props.header}</Head>;
+export const Header = (props) => {
+  return (
+    <HeaderBar>
+      <HeaderBarContent>
+        <HeaderTitle>Vertfarm</HeaderTitle>
+      </HeaderBarContent>
+    </HeaderBar>
+  );
+};
 
-const Head = styled.h1`
-  text-align: left;
-  margin: 10px;
+const HeaderBar = styled.div`
+  height: 85px;
+  width: 100%;
 
-  margin-left: 20px;
+  margin: 0px;
+
+  background: #${(p) => p.theme.colors.primaryColor};
+`;
+
+const HeaderBarContent = styled.div`
+  display: flex;
+  align-items: center;
+
+  height: 85px;
+  width: 90%;
+  max-width: 1000px;
+
+  margin: 0 auto;
+`;
+
+const HeaderTitle = styled.h1`
+  margin: 0px;
+
+  color: #${(p) => p.theme.colors.textColor};
 `;
