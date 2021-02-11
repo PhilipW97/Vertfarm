b996e94376ab2358c18b4f662246b6b06eb97298
Philip Willms <philip@narando.de>
2021-02-11 21:16:27 +0100

    feat: add theme provider

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const COLORS = {
+  textColor: "ffffff",
+  backgroundColor: "161616",
+  primaryColor: "1E1D20",
+};
+
+const SIZES = {};
+
+const MEDIA_QUERIES = {};
+
+export { COLORS, SIZES, MEDIA_QUERIES };
