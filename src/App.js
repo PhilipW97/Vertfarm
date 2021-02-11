6bc9b6b4f288e277cc3d157f3473c44cce19cb7b
Philip Willms <philip@narando.de>
2021-02-11 21:33:17 +0100

    refactor: remove unneccessary

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { Header } from "./components/Header";
 import { COLORS } from "./constants";
 
-import styled from "styled-components";
-
-const App = () => (
-  <ThemeProvider theme={{ colors: COLORS }}>
-  </ThemeProvider>
-);
+const App = () => <ThemeProvider theme={{ colors: COLORS }}></ThemeProvider>;
 
 export default App;
-
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: 100%;
-  background: white;
-  padding: 20px 0px;
-`;
