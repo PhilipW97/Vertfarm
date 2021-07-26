f491fadc2946153b77494c315e4f5ad8499f8882
Philip Willms <philip@narando.de>
2021-07-26 22:46:50 +0200

    move table into own component

---
+++ src/features/light/LightTable.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+export const LightTable = ({ lightData }) => {
+  return (
+    <TableWrapper>
+      <Table cellSpacing="0">
+        {lightData.map((light, index) => (
+          <tbody key={index}>
+            <TableRow>
+              <TableElement>{light.date}</TableElement>
+              <TableElement state={light.lightState}>
+                {light.lightState ? "ON" : "OFF"}
+              </TableElement>
+            </TableRow>
+          </tbody>
+        ))}
+      </Table>
+    </TableWrapper>
+  );
+};
+
+const TableWrapper = styled.div`
+  width: 100%;
+
+  margin-top: 20px;
+  overflow-y: scroll;
+`;
+
+const Table = styled.table`
+  width: 90%;
+
+  margin: 20px 10px;
+
+  color: #${(p) => p.theme.colors.primaryTextColor};
+`;
+
+const TableRow = styled.tr``;
+
+const TableElement = styled.td`
+  padding: 10px;
+  border-top: 1px solid #${(p) => p.theme.colors.primaryTextColor};
+
+  ${(p) => p.state !== undefined && `color: ${p.state ? "yellow" : "red"};`}
+`;
